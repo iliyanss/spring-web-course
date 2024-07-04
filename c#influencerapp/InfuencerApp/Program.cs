55588dcbf0ba4078777b23dd79d8484fe37b64a6
iliyanss <168529509+iliyanss@users.noreply.github.com>
2024-07-04 13:18:18 +0300

    Add files via upload
    basic c# app

---
+++ c#influencerapp/InfuencerApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using InfluencerManagerApp.Core;
+using InfluencerManagerApp.Core.Contracts;
+
+namespace InfluencerManagerApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IEngine iEngine = new Engine();
+        iEngine.Run();
+    }
+}
