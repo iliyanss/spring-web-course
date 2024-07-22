6c1ed4fab275de2993e7fa1a6c12f9b1e73eba74
iliyanss <iliyan.ss@outlook.com>
2024-07-22 14:29:08 +0300

    trying to add i18n internationalization

--- BuilderShop/src/main/java/bg/softuni/buildershop/config/WebConfig.java
+++ BuilderShop/src/main/java/bg/softuni/buildershop/config/WebConfig.java
@@ -1,8 +1,15 @@
 package bg.softuni.buildershop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +19,23 @@ public class WebConfig implements WebMvcConfigurer {
         // This maps the URL path /images/** to the local filesystem path C:/temp/
         registry.addResourceHandler("static/images/**")
                 .addResourceLocations("file:///C:/temp/");
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
